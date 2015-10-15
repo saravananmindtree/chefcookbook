8318de7d8931f2d5b7b814b5a4fd332f9f5ea66b
Ubuntu <radmin@automate.stxneauuechexczpdfk2ovstba.ix.internal.cloudapp.net>
2015-10-15 09:59:40 +0000

    First cookbook of PHP App stack

---
+++ .chef/knife.rb
@@ -0,0 +1,1 @@
+cookbook_path [ '/home/radmin/chef-repo/cookbooks' ]
---
+++ cookbooks/phpapp/metadata.rb
@@ -0,0 +1,7 @@
+name             'phpapp'
+maintainer       'Mindtree'
+maintainer_email 'saravanan.sampath@mindtree.com'
+license          'All rights reserved'
+description      'Installs/Configures phpapp'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
